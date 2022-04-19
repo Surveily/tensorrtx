82e7ad28a4625a766b13bb33d27129d09182ca48
Kacper Szufnarowski <kacper.szufnarowski@surveily.com>
2022-04-19 14:54:06 +0200

    added wts converter

---
+++ inception/inceptionv3/ToWTS.py
@@ -0,0 +1,15 @@
+import torch
+import struct
+net = torch.load("model.pt")
+
+f = open("inception.wts", 'w')
+f.write("{}\n".format(len(net.keys())))
+for k,v in net.items():
+    print('key: ', k)
+    print('value: ', v.shape)
+    vr = v.reshape(-1).cpu().numpy()
+    f.write("{} {}".format(k, len(vr)))
+    for vv in vr:
+        f.write(" ")
+        f.write(struct.pack(">f", float(vv)).hex())
+    f.write("\n")
